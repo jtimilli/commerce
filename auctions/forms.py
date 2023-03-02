c4d89b211a6d0c990d8b50bc2b1106dfbbbfbaa9
jtimilli <65932886+jtimilli@users.noreply.github.com>
2023-03-02 17:40:59 -0500

    add css to comment in listining html

--- auctions/forms.py
+++ auctions/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
-    content = forms.CharField(label="Comment", widget=forms.Textarea(attrs={'rows': 3}))
+    content = forms.CharField(label="Comment", widget=forms.Textarea(attrs={'rows': 2, "class": "form-control"}))
 
     class Meta:
         model = Comment
