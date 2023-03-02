8dcf540d4ac24147c5c9ef35decb88f3b7ae714c
jtimilli <65932886+jtimilli@users.noreply.github.com>
2023-03-02 12:48:15 -0500

    Added models to admin

--- auctions/models.py
+++ auctions/models.py
@@ -8,7 +8,7 @@ class AuctionListing(models.Model):
     description = models.TextField()
     starting_bid = models.DecimalField(max_digits=8, decimal_places=2)
     current_bid = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True, default=0)
-    image = models.ImageField(upload_to='media')
+    image = models.ImageField(upload_to='media', blank=True)
     category = models.CharField(max_length=255)
     date_listed = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
